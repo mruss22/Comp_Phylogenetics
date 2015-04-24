509bcf6c1f4c1e1d693f8ed59a2c0c74f825d641
mruss22 <mruss22@tigers.lsu.edu>
2015-04-24 11:12:54 -0500

    Update discrete_samp.py

--- discrete_samp.py
+++ discrete_samp.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 00:28:12 2015
+
+@author: Marisa_29
+"""
 #*** Discrete Sampling Practice ***
 
 """---> Creating useful functions <---
@@ -8,11 +14,12 @@ from __future__ import division #imports the division module
 import time
 
 def factorial(max,min):
+    #factorial definition: the product of an integer and all the integers below it
     product=1
     for i in range(max,min-1,-1):
         product = product * i
     return product
-print factorial(5,0)
+print factorial(5,1)
 
 """Ex. if n is 5 then the range will return the values 2,3,4,5 one by one with each iteration,
 and we multiply them with base in each iteration. """
@@ -35,9 +42,9 @@ there is a noticeable difference in speed between the (2a) and (2b) function.
 Which one is faster? By roughly how much?"""
 
 #2a & 3
-def binCoeff(n,k):
+def binom1(n,k):
     '''This function calculates the binomial coefficient for two integers n and k.'''
-    start = time.time()    
+    start = time.time()    #will return time in seconds
     if k > n: #establishes the value of the binomial coefficient if k > n
         return 0
     else: #calculates the binomial coefficient using the factorial formula for each element
@@ -45,12 +52,12 @@ def binCoeff(n,k):
         end = time.time()
         elapsed = end - start        
         return coeff, elapsed
-print binCoeff(500,2)
+print binom1(5000,2)
 
 #2b & 3
-def binCoeff2(n,k):
+def binom2(n,k):
     '''This function calculates the binomial coefficient for two integers n and k in a more concise way.'''
-    start = time.time()
+    start = time.time() #will return time in seconds
     if k > n: #establishes the value of the binomial coefficient if k > n
         return 0
     else: #calculates the binomial coefficient by cancelling out the common terms in both parts of the division
@@ -58,20 +65,24 @@ def binCoeff2(n,k):
         end = time.time()
         elapsed = end - start
         return coeff,elapsed
-print binCoeff2(500,2)
+print binom2(5000,2)
 
-def binCoeff3(n,k):
-    '''This function calculates the binomial coefficient for two integers n and k in a more concise way.'''
+print 'binom2 is faster than binom1'
+
+def binom3(n,k):
+    '''This function calculates the binomial coefficient for two integers n and k in a more concise way. Without calculating 
+    the return time'''
     if k > n: #establishes the value of the binomial coefficient if k > n
         return 0
     else: #calculates the binomial coefficient by cancelling out the common terms in both parts of the division
         coeff = factorial(n,(n-k+1))/factorial(k,1)    
         return coeff
-print binCoeff2(500,2)
+print binom3(5000,2)
 """Easier way would be to import the sympy module and use .binomial
 import sympy
 binomCoef=sympy.binomial(5, 2)
-print binomCoef"""
+print binom"""
+
 
 
 """(4) Use either function (2a) or (2b) to write a function that calculates the 
@@ -79,12 +90,13 @@ probability of k successes in n Bernoulli trials with probability p. This is
 called the Binomial(n,p) distribution. See Theorem 3.3.5 for the necessary equation. 
 [Hint: pow(x,y) returns x^y (x raised to the power of y).]"""
 
-def binomPMF(k,n,p):
+def binomPMF(k,n,p): #binomial probability mass function 
+#will use bionm3 (calculates binomial concisely without return time)
     """
     This function returns the probability mass of k successes for a binomial 
-    distribution with n trials and a probability of success given by p.
+    distribution with n Bernoulli trials and a probability of success given by p.
     """
-    return binCoeff3(n,k)*pow(p,k)*pow((1-p),(n-k))
+    return binom3(n,k)*pow(p,k)*pow((1-p),(n-k))
 print binomPMF(5,10,0.5)
 
 """(5) Now write a function to sample from an arbitrary discrete distribution. 
@@ -92,25 +104,20 @@ This function should take two arguments. The first is a list of arbitrarily
 labeled events and the second is a list of probabilities associated with these 
 events. Obviously, these two lists should be the same length."""
 
-#function taken from jembrown 
-import scipy ## Import scipy to use when drawing a random number b/t 0 and 1)    
-def discSamp(events,probs):
-    """
-    This function samples from a list of discrete events provided in the events argument, using the event
-    probabilities provided in the probs argument. These lists must:
-        - Be the same length
-        - Be in corresponding orders
-    Also, the probabilities in probs must sum to 1.
-    """
-    ranNum = scipy.random.random()
-    cumulProbs = []
-    cumulProbs.extend([probs[0]])
-    for i in range(1,len(probs)):
-        cumulProbs.extend([probs[i]+cumulProbs[-1]])
-    for i in range(0,len(probs)):
-        if ranNum < cumulProbs[i]:
-           return events[i]
-    return None
+def DiscreteSamp(events,probabilities):    
+    import random
+    x= random.choice(events) #chooses a random number from eventlist 
+    index=events.index(x)
+    #this indexes the random events 
+    probability=probabilities[index]
+    #this is the probabilities of the events given 
+    
+    return "this is the event", x, "and this is the probability", probability
+    
+eventlist=["1","2","3","4","5"]#the lists must be the same length
+problist=[.15,.45,.25,.05,.1]#events sum to 1
+print DiscreteSamp(eventlist,problist)
+
 
 """---> Sampling sites from an alignment <---
 
@@ -122,7 +129,31 @@ The second type supports the monophyly of taxon A and taxon C.
  sample a new alignment (a new set of site pattern counts) with replacement 
  from the original using your function from (5). Print out the counts of the 
  two types."""
- 
+sites = [0,400]
+for i in range(0,400):
+    sites.append(DiscreteSamp(problist, eventlist)) #using DiscreteSamp from (5)
+problist = [.5,.5] # 50% chance of either type 1 or type 2 
+eventlist = ["a", "b"]  #type 1 and type 2
+print(sites.count("a")) #counts amount of times type 1 appears
+print(sites.count("b")) #counts amount of times type  appears
+#argh can't get my code to work it keeps outputting 0 for both type 1 and type 2
+#will need to rewrite code in (5)?
+
+
+
+#using jembrown code (below) to complete 7-11 of excercise 
+import scipy ## Import scipy to use when drawing a random number b/t 0 and 1)    
+def discSamp(events,probs):
+    ranNum = scipy.random.random()
+    cumulProbs = []
+    cumulProbs.extend([probs[0]])
+    for i in range(1,len(probs)):
+        cumulProbs.extend([probs[i]+cumulProbs[-1]])
+    for i in range(0,len(probs)):
+        if ranNum < cumulProbs[i]:
+           return events[i]
+    return None
+
 sites = []												
  # Stores each of the 400 sites in each replicate draw.
 for siteIndex in range(0,400):								
@@ -131,92 +162,75 @@ for siteIndex in range(0,400):
  # Each new 'site' is added to sites
 print("Type 1 count: %d" % sites.count("type1"))
 print("Type 2 count: %d" % sites.count("type2"))
+ 
 
 """(7) Repeat (6) 100 times and store the results in a list."""
-
-print("Sampling 100 times...")
-
-# Importing the sys library to be able to flush output to the screen. This will ensure
-#   that the progress indicator (with replicate number) shows up in real time.
-
-import sys
-
+print("repat 100 times")
 counts = []													
-# List to store the number of 'successes' in each replicate.
+# Emplty list to store type 1 or type 2 counts
 for reps in range(0,100):										
-# For loop to produce 100 replicate draws.
-    if reps % 10 == 0:											
-    # This will be true every 10 replicates. % gives the remainder of reps/10.
-        print(reps),											
-        # Prints out the replicate number. Including the comma causes a space 
-        #(rather than a newline) to follow the printed value.
-    sys.stdout.flush()											
-    # The flush() method makes sure anything waiting to be printed to the screen
-    #is actually printed immediately.
     sites = []
-    for siteIndex in range(0,400):
+    for siteIndex in range(0,4000): #increased the range by 10X to sample "100" times
         sites.extend([discSamp(["type1","type2"],[0.5,0.5])])
-    counts.extend([sites.count('type1')])						
-    # Summarizing the number of successes for each replicate.
+print("Type 1 count: %d" % sites.count("type1"))
+print("Type 2 count: %d" % sites.count("type2"))
 
 """(8) Of those 100 trials, summarize how often you saw particular proportions 
 of type 1 vs. type 2""
+e.g 
+Type 1 count: 1987 (49.6%)
+Type 2 count: 2013 (50.3%)
+ 
+it was often close to 50% type1 and 50% type2 which makes sense because the more replicates you do the closer 
+you should get to the actual probabilities
 
 #(9) Calculate the probabilities of the proportions you saw in (8) using the 
 binomial probability mass function (PMF) from (4)."""
-freqs = []                         
- # A list to store frequencies of type 1 site proportions
-uniqCounts = list(set(counts))      
-# Uses set() to find the unique elements of counts, then turns it back into a list
-for count in uniqCounts:            
-# For each count, it adds a tuple to freqs. The first element contains the observed frequency across replicates and the 2nd element contains the exact binomial prob mass.
-    freqs.extend([(float(counts.count(count))/float(100),binomPMF(count,400,0.5))])
+def binom3(n,k):
+    '''This function calculates the binomial coefficient for two integers n and k in a more concise way. Without calculating 
+    the return time'''
+    if k > n: #establishes the value of the binomial coefficient if k > n
+        return 0
+    else: #calculates the binomial coefficient by cancelling out the common terms in both parts of the division
+        coeff = factorial(n,(n-k+1))/factorial(k,1)    
+        return coeff
 
-print("Proportion, frequency")     
- # A header to label the columns that are about to be printed
-for i in range(0,len(uniqCounts)):  
-    print (round(float(uniqCounts[i])/400,3),freqs[i][0])
+def binomPMF(k,n,p): #binomial probability mass function 
+#will use bionm3 (calculates binomial concisely without return time)
+    """
+    This function returns the probability mass of k successes for a binomial 
+    distribution with n Bernoulli trials and a probability of success given by p.
+    """
+    return binom3(n,k)*pow(p,k)*pow((1-p),(n-k))
+type1PMF= binomPMF (496, 1000, .5)
+print type1PMF
 
-print("Proportion, Probability Mass")
-for i in range(0,len(uniqCounts)):
-    print (round(float(uniqCounts[i])/400,3),round(freqs[i][1],3))
+type2PMF= binomPMF (503, 1000, .5)
+print type2PMF
+
+"""results
+type 1 0.0244313690947
+type 2 0.0247754728848"""
+
+"""print binomPMF(1987,4000,0.5)
+OverflowError: integer division result too large for a float"""
 
 #(10) Compare your results from (8) and (9).
-diffs100 = []                       # List to store differences between sampled frequencies of counts and probability masses of counts
-for f in freqs:
-    diffs100.extend([f[0]-f[1]])    # Iteratively calculates differences b/w obs and expected and adds to the list
 
-for i in range(0,2):                # Just printing a couple of lines to space things out
-	print("")
-print("Differences based on 100 resamplings...")
-print("")
-print " ".join('%0.4f' % diff for diff in diffs100) # Prints differences as one long line, by joining values with a space (" "). Formats floats to use 4 decimal places.
+#porportions are similar (as they should be) since they should be close to a 50/50 rati0
 
 #(11) Repeat 7-10, but use 10,000 trials.
-print("")
-print("")
-print("Sampling 10,000 times...")
-
-counts = []
-for reps in range(0,10000):
-    if reps % 1000 == 0:
-        print(reps),
-    sys.stdout.flush()
+print("repat 10,000 times")
+counts = []													
+# Emplty list to store type 1 or type 2 counts
+for reps in range(0,100):										
     sites = []
-    for siteIndex in range(0,400):
+    for siteIndex in range(0,400000): #increased the range by 10X to sample "10,000" times
         sites.extend([discSamp(["type1","type2"],[0.5,0.5])])
-    counts.extend([sites.count('type1')])
+print("Type 1 count: %d" % sites.count("type1"))
+print("Type 2 count: %d" % sites.count("type2"))
+"""results ....my computer hated that run....
+type 1 199,605 (49.9%)
+type 2 200,395 (50.1%)
 
-freqs = []
-uniqCounts = list(set(counts))
-for count in uniqCounts:
-    freqs.extend([(float(counts.count(count))/float(10000),binomPMF(count,400,0.5))])
-diffs10K = []
-for f in freqs:
-    diffs10K.extend([f[0]-f[1]])
-
-for i in range(0,2):
-	print("")
-print("Differences based on 10,000 resamplings...")
-print("")
-print " ".join('%0.4f' % diff for diff in diffs10K)
+The higher the number of reps the closer to the 50/50 value that it should be"""
